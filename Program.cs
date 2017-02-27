871e4a94901abf7361b9b6b2b29e8d663e24aca9
DESKTOP-8N4O58I\smkn12jkt{4} <mukti.anggoro@gmail.com>
2017-02-27 15:58:55 +0700

    upload percobaan

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+using System.ComponentModel.DataAnnotations;namespace ClMvcD13.Model
+{
+    public class Program
+    {
+        [Key]
+        public int ProgramID { get; set; }
+        [MaxLength(15)]
+        public string ProgramName { get; set; }
+        public int JurusanID { get; set; }
+        public Jurusan Jurusan {get;set; }
+        public ICollection<Siswa> Siswas { get; set; }
+    }
+}
